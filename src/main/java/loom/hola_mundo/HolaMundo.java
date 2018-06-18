fce441dfd70662394f82d2d1366d683d5360f15f
Jonathan Fernández <jony.a.fernandez@gmail.com>
2018-06-18 15:19:31 -0300

    Resolucion del ejercicios

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -2,7 +2,15 @@ package loom.hola_mundo;
 
 public class HolaMundo {
 
+	/**
+	 * Metodo para saludar
+	 * @param persona Nombre de la persona a la que se saludará
+	 * @return saludo.
+	 */
+	
     public String saludar(final String persona) {
-        throw new RuntimeException("¡Implementar este método!");
+        if(persona.isEmpty())
+        	return "¡Hola!";
+        return "¡Hola, "+persona+"!";
     }
 }
